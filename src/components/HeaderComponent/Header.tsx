46399b1dbdd3374164d776d0c05c2fb02bf0b194
pedrohfideeelis <phzimnerdola@gmail.com>
2024-07-01 15:50:10 -0300

    add header and statscards components

--- src/components/HeaderComponent/Header.tsx
+++ src/components/HeaderComponent/Header.tsx
@@ -1,16 +1,24 @@
+import { StatsCards } from "../StatsCardsComponent/StatsCards";
 import styles from "./styles.module.scss";
 
-export const Header: React.FC = () => {
-  return(
-    <header>
-        <div className={styles.infos}>
-            <h1>MyTodo</h1>
-            <span>Bem vindo {}</span>
-        </div>
+interface HeaderProps {
+  userName: string;
+}
 
-        <div className="cards">
-            {/*CARDS*/}
+export const Header: React.FC<HeaderProps> = ({userName}) => {
+  return (
+    <header className={styles.infos}>
+      <div className={styles.container}>
+        <div>
+          <h1>MyTodo</h1>
+          <span>Bem vindo {userName}</span>
         </div>
+        <div className="cards">
+          <StatsCards title="Total de Tarefas" value={5}/>
+          <StatsCards title="Tarefas Pendentes" value={4}/>
+          <StatsCards title="Tarefas Concluídas" value={3}/>
+        </div>
+      </div>
     </header>
   );
 };
