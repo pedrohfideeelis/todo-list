032b09bfe77e3d941ea69b8616a6bb20df6ec129
pedrohfideeelis <phzimnerdola@gmail.com>
2024-07-04 12:54:35 -0300

    update taskComponent

--- src/components/TasksComponent/Tasks.tsx
+++ src/components/TasksComponent/Tasks.tsx
@@ -1,11 +1,37 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import styles from "./styles.module.scss";
+
+interface Task {
+  title: string;
+  done: boolean;
+  id: number;
+}
 
 export const Tasks: React.FC = () => {
   const [taskTitle, settaskTitle] = useState("");
+  const [tasks, setTasks] = useState([] as Task[]);
+
+  function handleSubmitAddTask(event: FormEvent) {
+    event.preventDefault();
+
+    if (taskTitle.length < 3) {
+      alert(
+        "Número de caracteres inválido, a tarefa não pode ter menos de 3 caracteres"
+      );
+      return;
+    }
+
+    setTasks([
+      { id: new Date().getTime(), title: taskTitle, done: false },
+      ...tasks,
+    ]);
+
+    settaskTitle("");
+  }
+
   return (
     <section className={styles.container}>
-      <form>
+      <form onSubmit={handleSubmitAddTask}>
         <div>
           <label htmlFor="task-title">Adicionar Tarefa</label>
           <input
@@ -16,8 +42,19 @@ export const Tasks: React.FC = () => {
             placeholder="Título da Tarefa"
           />
         </div>
-        <button>Adicionar Tarefa</button>
+        <button type="submit">Adicionar Tarefa</button>
       </form>
+
+      <ul>
+        {tasks.map((task) => {
+          return (
+            <li key={task.id}>
+              <input type="checkbox" id={`task-${task.id}`} />
+              <label htmlFor={`task-${task.id}`}>{task.title}</label>
+            </li>
+          );
+        })}
+      </ul>
     </section>
   );
 };
